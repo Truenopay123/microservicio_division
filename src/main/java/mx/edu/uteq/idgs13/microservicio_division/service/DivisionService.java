10211eadb541c2518172fe6d40e4292f3748c549
2023171018 <2023171018@uteq.edu.mx>
2025-10-03 18:33:51 -0600

    Creacion de BackEnd para editar division y buscar por ID

--- src/main/java/mx/edu/uteq/idgs13/microservicio_division/service/DivisionService.java
+++ src/main/java/mx/edu/uteq/idgs13/microservicio_division/service/DivisionService.java
@@ -37,4 +37,19 @@ public class DivisionService {
         return resultado;
     }
 
+    public Division findById(Long id) throws Exception {
+        return divisionRepository.findById(id)
+            .orElseThrow(() -> new Exception("División no encontrada con id: " + id));
+    }
+
+    public Division updateDivision(Long id, String nombre, boolean activo) throws Exception {
+        Division division = divisionRepository.findById(id)
+            .orElseThrow(() -> new Exception("División no encontrada con id: " + id));
+        
+        division.setNombre(nombre);
+        division.setActivo(activo);
+        
+        return divisionRepository.save(division);
+    }
+
 }
